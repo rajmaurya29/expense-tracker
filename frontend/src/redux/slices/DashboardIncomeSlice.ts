d6c453890c6e983f63ed2d73dea0e8f20c9dd916
Raj Maurya <rajmaurya29092004@gmail.com>
2025-11-12 01:02:12 +0530

    removed unused imports

--- frontend/src/redux/slices/DashboardIncomeSlice.ts
+++ frontend/src/redux/slices/DashboardIncomeSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice,createAsyncThunk } from '@reduxjs/toolkit'
-import axios from 'axios'
+// import axios from 'axios'
 import api from '../../../utils/api'
 import { getFilterParams } from '../../../utils/getFilterParams';
 // const API_URL = import.meta.env.VITE_API_URL as string;
