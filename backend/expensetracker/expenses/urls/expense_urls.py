be4f98e83cedd02a9d428dfd97ece43df1e1264e
Raj Maurya <rajmaurya29092004@gmail.com>
2025-11-14 10:35:38 +0530

    change some url

--- backend/expensetracker/expenses/urls/expense_urls.py
+++ backend/expensetracker/expenses/urls/expense_urls.py
@@ -3,8 +3,8 @@ from expenses.views import expense_views
 
 urlpatterns = [
     path('',expense_views.expense,name="create_get_edit_expense"),
-    path('<int:id>',expense_views.individual_expense,name='get_delete_expense'),
     path('expenseCategory/',expense_views.expense_category,name='expense_category'),
     path('transactions/',expense_views.recentTransactionsExpense,name='recent_Transactions'),
     path('transactions/csv/',expense_views.export_csv,name='export-csv'),
+    path('<int:id>',expense_views.individual_expense,name='get_delete_expense'),
 ]   
--- backend/expensetracker/expenses/urls/income_urls.py
+++ backend/expensetracker/expenses/urls/income_urls.py
@@ -3,9 +3,9 @@ from expenses.views import income_views
 
 urlpatterns = [
     path('',income_views.income,name="create_get_edit_income"),
-    path('<int:id>',income_views.individual_income,name='get_delete_income'),
     path('categoryIncome/',income_views.income_category,name='income_category'),
     path('transactions/',income_views.recentTransactionsIncome,name='recentTransactions'),
     path('transactions/csv/',income_views.export_csv,name='export-csv'),
+    path('<int:id>',income_views.individual_income,name='get_delete_income'),
 
 ]
